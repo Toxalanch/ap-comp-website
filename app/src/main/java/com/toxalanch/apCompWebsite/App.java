4fde9288806c807cc8ead9aa0d2e62e26b4b8162
Toxalanch <gregory@arnoldzoo.org>
2026-02-11 10:45:44 -0600

    Deleted doGet as it was unused

--- app/src/main/java/com/toxalanch/apCompWebsite/App.java
+++ app/src/main/java/com/toxalanch/apCompWebsite/App.java
@@ -1,7 +1,6 @@
 package com.toxalanch.apCompWebsite;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -18,12 +17,6 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/mainServlet")
 public class App extends HttpServlet{
     private static final long serialVersionUID = 1L;
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter out = response.getWriter();
-        out.println("Hello world");
-    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
