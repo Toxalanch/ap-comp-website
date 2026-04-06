f4c902fd8f699bebeff5bf172d6a153a198a45d5
Toxalanch <gregory@arnoldzoo.org>
2026-04-06 10:58:53 -0500

    Creates a tester for the AppUtil Class

---
+++ app/src/test/java/com/toxalanch/apCompWebsite/AppUtilTester.java
@@ -0,0 +1,16 @@
+package com.toxalanch.apCompWebsite;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AppUtilTester {
+
+    @Test
+    void findClass() {
+        assertEquals("Hello", AppUtil.findClass("public class Hello {"));
+        assertEquals("Hello", AppUtil.findClass("public class Hello{"));
+        // assertEquals("Hello", AppUtil.findClass("class Hello{"));    Doesn't currently work, but I need to make it work later
+    }
+    
+}
