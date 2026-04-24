3ef2c4624ae0c385c0cbc0ffa3128dd9bbb39a18
Toxalanch <gregory@arnoldzoo.org>
2026-04-24 10:59:47 -0500

    Revert "test"
    This reverts commit 363009c1bfa5eafbce5d8d7d09a253e709f1ccc7.

--- app/src/test/java/com/toxalanch/apCompWebsite/AppUtilTester.java
+++ app/src/test/java/com/toxalanch/apCompWebsite/AppUtilTester.java
@@ -1,7 +1,6 @@
 package com.toxalanch.apCompWebsite;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +9,7 @@ public class AppUtilTester {
     @Test
     void findClass() {
         assertEquals("Hello", AppUtil.findClass("public class Hello {"));
-        assertNotEquals("Hello", AppUtil.findClass("public class Hello{"));
+        assertEquals("Hello", AppUtil.findClass("public class Hello{"));
         //TODO assertEquals("Hello", AppUtil.findClass("class Hello{"));    Doesn't currently work, but I need to make it work later
     }
     
